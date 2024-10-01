eb8f90fdb89172ad93c32d9187b41077677aafae
GeovanniHdez <jhernandeza38@miumg.edu.gt>
2024-10-01 11:47:39 -0600

    push 1
    push 1

---
+++ vistas/home.php
@@ -0,0 +1,5 @@
+<div class="container is-fluid">
+	<h1 class="title">Home</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	<img src="./img/logo3.png" width="30" height="28">
+</div>
