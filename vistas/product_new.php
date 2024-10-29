8ade787c16996eb0c032549e6053b80ec22760f6
evasquezm <eduvan.emerson.duvan@gmail.com>
2024-10-28 21:53:21 -0600

    Actualizacion

--- vistas/product_new.php
+++ vistas/product_new.php
@@ -200,7 +200,7 @@
 		</div>
 		<div class="columns">
 			<div class="column">
-				<label><strong>Foto o imagen del producto</strong></label><br>
+				<label>Foto o imagen del producto</label><br>
 				<div class="file is-small has-name">
 				  	<label class="file-label">
 				    	<input class="file-input" type="file" name="producto_foto" accept=".jpg, .png, .jpeg" >
